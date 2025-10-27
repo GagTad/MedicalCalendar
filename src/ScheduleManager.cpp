5558bfa75cf822b5a831117b0be9ddb529f46ce4
GagTad <136594120+GagTad@users.noreply.github.com>
2025-10-28 00:20:11 +0400

    Update ScheduleManager.cpp

--- src/ScheduleManager.cpp
+++ src/ScheduleManager.cpp
@@ -1,30 +1,41 @@
 #include "ScheduleManager.h"
+#include <iostream>
+#include "Doctor.h"
+#include "Patient.h"
 
 ScheduleManager::ScheduleManager()
 {
 
 }
 
-bool ScheduleManager::addAppointment(TimePoint start, TimePoint end, const std::string& desc)
+bool ScheduleManager::addAppointment(TimePoint start, TimePoint end, const Doctor* doc, const Patient* pat, const std::string& desc)
 {
-	Appointment newAppointment(start, end, desc);
-	
-	for (const auto& existingAppointmentPtr : allAppointments)
+	Appointment newAppointment(start, end,doc ,pat , desc);
+	if (!allAppointments.size())
 	{
-		if (newAppointment.overlapsWith(*existingAppointmentPtr)) 
-		{
-			std::cout << "[ERROR] Conflict Detected: Cannot add:" << desc << std::endl;
-			std::cout << "Its overlaps with ";
-			existingAppointmentPtr->print();
-			return false;
-		}
-
-		allAppointments.push_back(std::make_unique<Appointment>(start, end, desc));
-		std::cout << "[INFO] Successfully added :" << std::endl;
+		allAppointments.push_back(std::make_unique<Appointment>(start, end,doc ,pat, desc));
+		std::cout << "[INFO] Successfully added :" << desc << std::endl;
 		return true;
 	}
 	
-	std::cout << "New Appointemnt added ----:" << desc << std::endl;
+	for (const auto& existingAppointmentPtr : allAppointments)
+	{
+		if(existingAppointmentPtr->getDoctor()->getId() == doc->getId()){
+			if (newAppointment.overlapsWith(*existingAppointmentPtr)) 
+			{
+				std::cout << "[ERROR] Conflict Detected: Cannot add:" << desc << std::endl;
+				std::cout << "Its overlaps with ";
+				existingAppointmentPtr->print();
+				return false;
+			}
+		}
+
+		allAppointments.push_back(std::make_unique<Appointment>(start, end,doc , pat,  desc));
+		std::cout << "[INFO] Successfully added :" << desc << std::endl;
+		return true;
+	}
+	
+
 }
 
 void ScheduleManager::printAllAppointments() const
