8ce0c9ea75fa0f96f8192b745ef4cc2f304138d8
GagTad <136594120+GagTad@users.noreply.github.com>
2025-10-28 00:19:46 +0400

    Update Appointment.cpp

--- src/Appointment.cpp
+++ src/Appointment.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "Appointment.h"
+#include "Doctor.h"
+#include "Patient.h"
 
-Appointment::Appointment(TimePoint start, TimePoint end, std::string desc)
-	: startTime(start), endTime(end), description(desc){}
+Appointment::Appointment(TimePoint start, TimePoint end, const Doctor* doc, const Patient* pat, std::string desc)
+	: startTime(start), endTime(end), doctor(doc), patient(pat), description(desc){}
 
 void Appointment::print() const {
 	auto duration = std::chrono::duration_cast<std::chrono::minutes>(endTime - startTime);
 
 	std::cout << "---Appointment Details---" << std::endl;
+	std::cout << "Doctor: " << doctor->getName()<< std::endl;
+	std::cout << "Patient: " << patient->getName() << std::endl;
 	std::cout << "Description: " << description << std::endl;
 	std::cout << "Duration: " << duration.count() << " minutes" << std::endl;
 	std::cout << "-------------------------" << std::endl;
