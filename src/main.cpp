5ab9e53f5439b98bed22c861580888ac19596f83
GagTad <136594120+GagTad@users.noreply.github.com>
2025-10-27 23:21:32 +0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,16 @@ int main()
 
 	TimePoint now = std::chrono::system_clock::now();
 
-	manager.addAppointment(now, now + std::chrono::minutes(45), "Consultation with Dr. Avagyan");
-	
-	TimePoint later = now + std::chrono::hours(2);
-	
-	manager.addAppointment(later, later + std::chrono::minutes(30), "Dental check-up with Dr. Petrosyan");
-
-	manager.addAppointment(now + std::chrono::hours(4), now + std::chrono::hours(5), "Meeting with the administrator");
-
+	manager.addAppointment(now, now + std::chrono::minutes(60), "Consultation with Dr. Avagyan"); //0-60 min
+	std::cout << std::endl;
+	manager.addAppointment(now + std::chrono::minutes(90), now + std::chrono::minutes(120), "Dental check-up with Dr. Petrosyan"); //  90-120 min
+	std::cout << std::endl;
+	manager.addAppointment(now + std::chrono::minutes(30), now + std::chrono::minutes(75), "CONFLICT A (30-75 min)");
+	std::cout << std::endl;
+	manager.addAppointment(now + std::chrono::minutes(15), now + std::chrono::minutes(45), "CONFLICT B (15-45 min)");
+	std::cout << std::endl;
+	manager.addAppointment(now + std::chrono::minutes(60), now + std::chrono::minutes(90), "Meeting 3 (60-90 min)");
+	std::cout << std::endl;
 	manager.printAllAppointments();
 
 	return 0;
