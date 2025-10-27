36c4050d461e90216291f2ba06be01a163cd116a
GagTad <136594120+GagTad@users.noreply.github.com>
2025-10-27 22:55:33 +0400

    Modify addAppointment to return success status
    Changed the addAppointment method to return a boolean indicating success or failure. Updated appointment creation and conflict detection logic.

--- src/ScheduleManager.cpp
+++ src/ScheduleManager.cpp
@@ -5,11 +5,24 @@ ScheduleManager::ScheduleManager()
 
 }
 
-void ScheduleManager::addAppointment(TimePoint start, TimePoint end, const std::string& desc)
+bool ScheduleManager::addAppointment(TimePoint start, TimePoint end, const std::string& desc)
 {
-	auto newAppointment = std::make_unique<Appointment>(start, end, desc);
+	Appointment newAppointment(start, end, desc);
+	
+	for (const auto& existingAppointmentPtr : allAppointments)
+	{
+		if (newAppointment.overlapsWith(*existingAppointmentPtr)) 
+		{
+			std::cout << "[ERROR] Conflict Detected: Cannot add:" << desc << std::endl;
+			std::cout << "Its overlaps with ";
+			existingAppointmentPtr->print();
+			return false;
+		}
 
-	allAppointments.push_back(std::move(newAppointment));
+		allAppointments.push_back(std::make_unique<Appointment>(start, end, desc));
+		std::cout << "[INFO] Successfully added :" << std::endl;
+		return true;
+	}
 	
 	std::cout << "New Appointemnt added ----:" << desc << std::endl;
 }
