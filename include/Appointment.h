48bc2487507cf5d98852b9ce7cc81df5afa9d3a9
GagTad <136594120+GagTad@users.noreply.github.com>
2025-10-28 00:21:27 +0400

    Update Appointment.h

--- include/Appointment.h
+++ include/Appointment.h
@@ -4,17 +4,22 @@
 #include <string>
 #include <chrono>
 
+class Doctor;
+class Patient;
 
 using TimePoint = std::chrono::system_clock::time_point;
 
 class Appointment {
 
 public:
-	Appointment(TimePoint start, TimePoint end, std::string desc);
+	Appointment(TimePoint start, TimePoint end,const Doctor* doc, const Patient* pat, std::string desc);
+	
 	void print() const;
 
 	TimePoint getStartTime() const { return startTime; }
 	TimePoint getEndTime() const { return endTime; }
+
+	const Doctor* getDoctor() const { return doctor; }
 
 	bool overlapsWith(const Appointment& other) const;
 
@@ -24,5 +29,7 @@ private:
 	TimePoint startTime;
 	TimePoint endTime;
 	std::string description;
+	const Doctor* doctor;
+	const Patient* patient;
 
 };
